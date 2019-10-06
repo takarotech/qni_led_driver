59cda2b5e76770dddc43f268398a815b2d246085
arduino12 <arduino12@users.noreply.github.com>
2019-10-06 14:30:08 +0300

    Add qni_v0 support

--- run_qni_led_driver.sh
+++ run_qni_led_driver.sh
@@ -10,5 +10,6 @@ fi
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 #-> Run qni_led_driver with arguments
+#sudo rpi-fb-matrix/rpi-fb-matrix qni_v0.cfg --led-pixel-mapper=Snake:2 --led-pwm-bits=8 --led-brightness=90 --led-pwm-lsb-nanoseconds 400 "$@"
 #sudo rpi-fb-matrix/rpi-fb-matrix qni_v1.cfg --led-gpio-mapping=free-i2c --led-multiplexing=4 --led-pixel-mapper=Snake:3 --led-pwm-bits=8 --led-brightness=90 --led-slowdown-gpio=2 --led-pwm-lsb-nanoseconds 500 "$@"
 sudo rpi-fb-matrix/rpi-fb-matrix qni_v2.cfg --led-gpio-mapping=free-i2c --led-multiplexing=4 --led-pixel-mapper=Snake:2 --led-pwm-bits=8 --led-brightness=90 --led-slowdown-gpio=3 --led-pwm-lsb-nanoseconds 500 "$@"

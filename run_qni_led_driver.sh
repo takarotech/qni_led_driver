e3b6468611ea009833fccff125a3aa075a53efaa
arduino12 <arduino12@users.noreply.github.com>
2019-10-06 10:50:44 +0300

    Fix LEDs blinking

--- run_qni_led_driver.sh
+++ run_qni_led_driver.sh
@@ -10,5 +10,5 @@ fi
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 #-> Run qni_led_driver with arguments
-#sudo rpi-fb-matrix/rpi-fb-matrix qni_v1.cfg --led-gpio-mapping=free-i2c --led-multiplexing=4 --led-pixel-mapper=Snake:3 --led-pwm-bits=8 --led-brightness=70 --led-slowdown-gpio=2 "$@"
-sudo rpi-fb-matrix/rpi-fb-matrix qni_v2.cfg --led-gpio-mapping=free-i2c --led-multiplexing=4 --led-pixel-mapper=Snake:2 --led-pwm-bits=8 --led-brightness=70 --led-slowdown-gpio=3 "$@"
+#sudo rpi-fb-matrix/rpi-fb-matrix qni_v1.cfg --led-gpio-mapping=free-i2c --led-multiplexing=4 --led-pixel-mapper=Snake:3 --led-pwm-bits=8 --led-brightness=90 --led-slowdown-gpio=2 --led-pwm-lsb-nanoseconds 500 "$@"
+sudo rpi-fb-matrix/rpi-fb-matrix qni_v2.cfg --led-gpio-mapping=free-i2c --led-multiplexing=4 --led-pixel-mapper=Snake:2 --led-pwm-bits=8 --led-brightness=90 --led-slowdown-gpio=3 --led-pwm-lsb-nanoseconds 500 "$@"

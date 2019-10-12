9082bd850f19fa00b8bd49c9b1ed1d30930a4e8c
arduino12 <arduino12@users.noreply.github.com>
2019-10-12 19:33:01 +0300

    Limit display resolution for driver

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 #-> Make sure we don't run as root
 if (( EUID == 0 )); then
-   echo 'Please run without sudo!' 1>&2
-   exit 1
+	echo 'Please run without sudo!' 1>&2
+	exit 1
 fi
 
 #-> Go to the directory of this script
@@ -35,6 +35,10 @@ sudo sed -i '1s/^/isolcpus=3 /' /boot/cmdline.txt
 
 #-> Remove unnecessary processes
 sudo apt remove -y bluez bluez-firmware pi-bluetooth triggerhappy pigpio
+
+#-> Limit resolution because 57=1680x1050 is too high for this driver,
+#   can set to 47=1440x900, 35=1280x1024 4=640x480
+sudo raspi-config nonint do_resolution 2 47
 
 #-> Add to autostart
 echo @$QNI_LED_DRIVER_DIR/run_qni_led_driver.sh >> $HOME/.config/lxsession/LXDE-pi/autostart
